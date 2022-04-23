6a892b8608853c6dc349a8c4445bd8314cbae195
tbabs <3014160476@qq.com>
2022-04-23 15:49:36 +0800

    第三次作业提交

--- wuys/src/main/java/com/tbabs/controller/UserController.java
+++ wuys/src/main/java/com/tbabs/controller/UserController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-import java.net.http.HttpResponse;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class UserController {
@@ -36,5 +36,41 @@ public class UserController {
         String res = "User{id = " + user.getId()+", name=" + user.getName() + ", sex=" + user.getSex() + ", age=" + user.getAge() + ", birthday="+ birthday +  "}";
         System.out.println(res);
         return res;
+    }
+
+    @RequestMapping(value = "/insertUser", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String insertUser(Integer id, String name, String sex, Integer age) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setSex(sex);
+        user.setAge(age);
+        user.setBirthday(new Date());
+        Integer row = userService.insertUser(user);
+        System.out.println("添加是否成功：" + (row == 1 ? "成功" : "失败"));
+        return "添加是否成功：" + (row == 1 ? "成功" : "失败");
+    }
+
+    @RequestMapping(value = "/deleteUser", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String deleteUser(Integer id) {
+        Integer row = userService.deleteUser(id);
+        System.out.println("删除是否成功：" + (row == 1 ? "成功" : "失败"));
+        return "删除是否成功：" + (row == 1 ? "成功" : "失败");
+    }
+
+    @RequestMapping(value = "/updateUser", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String updateUser(Integer id, String name, String sex, Integer age) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setSex(sex);
+        user.setAge(age);
+        user.setBirthday(new Date());
+        Integer row = userService.updateUser(user);
+        System.out.println("修改是否成功：" + (row == 1 ? "成功" : "失败"));
+        return "修改是否成功：" + (row == 1 ? "成功" : "失败");
     }
 }
--- wuys/src/main/java/com/tbabs/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.tbabs.dao;
-
-
-import com.tbabs.pojo.User;
-
-public interface UserDao {
-    User selectById(Integer id);
-}
